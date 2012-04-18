2b99ddf93d8750c594d24d64143aa0c4eda56d12
Moinax <moinax@gmail.com>
2012-04-18 15:03:09 +0200

    Removing new methods as they don't fetch our requirements

--- Entity/TranslatableEntityInterface.php
+++ Entity/TranslatableEntityInterface.php
@@ -3,24 +3,6 @@ namespace Snowcap\CoreBundle\Entity;
 
 interface TranslatableEntityInterface {
     /**
-     * Retrieve the current translation set
-     * Used to get the translation based on the current locale
-     *
-     * @abstract
-     * @return TranslationEntityInterface
-     */
-    public function getTranslated();
-
-    /**
-     * Set the current translation
-     * Used to set the translation based on the current locale
-     *
-     * @abstract
-     * @param TranslationEntityInterface $translation
-     */
-    public function setTranslated($translation);
-
-    /**
      * Get all translations for the entity
      *
      * @abstract
