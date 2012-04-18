19f29cbb9c239a8231fd9ec48f7e4ae41b195e1b
Moinax <moinax@gmail.com>
2012-04-18 14:07:07 +0200

    Adding new methods to get the current translation

--- Entity/TranslatableEntityInterface.php
+++ Entity/TranslatableEntityInterface.php
@@ -3,12 +3,33 @@ namespace Snowcap\CoreBundle\Entity;
 
 interface TranslatableEntityInterface {
     /**
+     * Retrieve the current translation set
+     * Used to get the translation based on the current locale
+     *
+     * @abstract
+     * @return TranslationEntityInterface
+     */
+    public function getTranslated();
+
+    /**
+     * Set the current translation
+     * Used to set the translation based on the current locale
+     *
+     * @abstract
+     * @param TranslationEntityInterface $translation
+     */
+    public function setTranslated($translation);
+
+    /**
+     * Get all translations for the entity
+     *
      * @abstract
      * @return Collection
      */
     public function getTranslations();
 
     /**
+     * Set all translations for the entity
      * @abstract
      * @param \Doctrine\Common\Collections\Collection $translations
      */
