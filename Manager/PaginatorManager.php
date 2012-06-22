d3c875ec69e1427f38343d49c60c0ff127a84a3c
Pierre Vanliefland <pierre@snowcap.be>
2012-06-22 16:21:15 +0200

    Added range limit option to paginator

--- Manager/PaginatorManager.php
+++ Manager/PaginatorManager.php
@@ -23,6 +23,13 @@ class PaginatorManager
     private $limitPerPage;
 
     /**
+     * The number of page links shown in the paginator widget
+     *
+     * @var int
+     */
+    private $limitRange;
+
+    /**
      * @var int
      */
     private $count;
@@ -94,7 +101,7 @@ class PaginatorManager
      */
     public function getCount()
     {
-        if(!isset($this->count)) {
+        if (!isset($this->count)) {
             $this->count = Paginate::getTotalQueryResults($this->query);
         }
         return $this->count;
@@ -108,10 +115,9 @@ class PaginatorManager
     public function getResult()
     {
         // First we need to check if there are actual results
-        if($this->getCount() > 0) {
+        if ($this->getCount() > 0) {
             $query = Paginate::getPaginateQuery($this->query, $this->getOffset(), $this->limitPerPage);
-        }
-        else {
+        } else {
             $query = $this->query;
         }
 
@@ -142,5 +148,43 @@ class PaginatorManager
         //Calculate offset for items based on current page number
         return ($this->page - 1) * $this->limitPerPage;
     }
+
+    /**
+     * @param int $limitRange
+     */
+    public function setLimitRange($limitRange)
+    {
+        $this->limitRange = $limitRange;
+    }
+
+    /**
+     * Return a range array that can be used to build pagination links
+     * The returned range is centered around the current page
+     *
+     * @return array
+     */
+    public function getRange()
+    {
+        if ($this->getNumPages() < $this->limitRange) { // Not enough pages to apply range limit
+            $start = 1;
+            $stop = $this->getNumPages();
+        } else { // Enough page to apply range limit
+            if($this->getPage() <= $this->limitRange / 2) { // Cannot center, current page too far on the left
+                $start = 1;
+                $stop = $start + $this->limitRange - 1;
+            }
+            elseif($this->getPage() + ceil($this->limitRange / 2) > $this->getNumPages()) { // Cannot center, current page too far on the right
+                $stop = $this->getNumPages();
+                $start = $stop - $this->limitRange + 1;
+            }
+            else { // Enough space on both sides, we can center
+                $start = $this->getPage() - floor($this->limitRange / 2) + 1;
+                $stop = $start + $this->limitRange -1;
+            }
+        }
+
+        return range($start, $stop);
+    }
+
 
 }
