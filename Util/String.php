313bad8c525b61dafe48b9a7b6f528c43786da38
pvanliefland <pierre@snowcap.be>
2012-11-07 22:10:57 +0100

    Added string util class (with camelize / underscore)

---
+++ Util/String.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Snowcap\CoreBundle\Util;
+
+class String {
+    /**
+     * Camelizes a string.
+     *
+     * @param string $id A string to camelize
+     *
+     * @return string The camelized string
+     */
+    public static function camelize($id)
+    {
+        return preg_replace_callback('/(^|_|\.)+(.)/', function ($match) { return ('.' === $match[1] ? '_' : '').strtoupper($match[2]); }, $id);
+    }
+
+    /**
+     * A string to underscore.
+     *
+     * @param string $id The string to underscore
+     *
+     * @return string The underscored string
+     */
+    public static function underscore($id)
+    {
+        return strtolower(preg_replace(array('/([A-Z]+)([A-Z][a-z])/', '/([a-z\d])([A-Z])/'), array('\\1_\\2', '\\1_\\2'), strtr($id, '_', '.')));
+    }
+}
