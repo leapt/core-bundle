89c0ec58d4ba8d577edf74dc9746319ccb02b414
pvanliefland <pierre@snowcap.be>
2012-11-07 16:14:35 +0100

    Adapted collection javascript to Symfony2.1 Added collection extension to manage add labels

---
+++ Form/Extension/CollectionTypeExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Snowcap\CoreBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+
+class CollectionTypeExtension extends AbstractTypeExtension {
+    /**
+     * @return string
+     */
+    public function getExtendedType()
+    {
+        return 'collection';
+    }
+
+    /**
+     * @param \Symfony\Component\OptionsResolver\OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'add_label' => 'Add an element'
+        ));
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormView $view
+     * @param \Symfony\Component\Form\FormInterface $form
+     * @param array $options
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['add_label'] = $options['add_label'];
+    }
+}
--- Resources/public/js/all.js
+++ Resources/public/js/all.js
@@ -1,29 +1,27 @@
 jQuery(function ($) {
 
-    var CollectionForm = function (container) {
+    var CollectionForm = function (widget) {
         var self = this;
-        var container = $(container);
-        var addButton = $('<a href="#" class="btn btn-primary">+</a>');
-        container.after(addButton);
+        var widget = $(widget);
+        var container = $(widget).parent('.collection-container');
         // When the link is clicked we add the field to input another element
 
-        self.removeElementForm = function(event){
+        self.removeElement = function(event){
             event.preventDefault();
             $(this).parent().remove();
         };
 
-        self.addElementForm = function(event){
+        self.addElement = function(event){
             event.preventDefault();
-            var prototype = container.attr('data-prototype');
-            var form = $(prototype.replace(/__name__/g, container.children().length));
+            var prototype = widget.attr('data-prototype');
+            var form = $(prototype.replace(/__name__/g, widget.children().length));
             var removeButton = form.find('.remove-element');
-            removeButton.on('click', self.removeElementForm);
-            form.prepend(removeButton);
-            container.append(form);
+            removeButton.on('click', self.removeElement);
+            widget.append(form);
         };
 
-        addButton.on('click', self.addElementForm);
-        container.find('.remove-element').on('click', self.removeElementForm);
+        container.find('.add-element').on('click', self.addElement);
+        widget.find('.remove-element').on('click', self.removeElement);
     };
     $.fn.collectionForm = function (options) {
         return this.each(function () {
