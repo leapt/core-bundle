7eea045d284a2207b9683f9be2fd304033e2aa3f
pvanliefland <pierre@snowcap.be>
2012-11-18 19:29:25 +0100

    Added slug validator

---
+++ Validator/Constraints/Slug.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Snowcap\CoreBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraints\Regex;
+
+/**
+ * @Annotation
+ */
+class Slug extends Regex
+{
+    /**
+     * @var string
+     */
+    public $message = 'A slug can only contain lowercase letters, numbers and hyphens';
+
+    /**
+     * @var string
+     */
+    public $pattern = '/([^a-z0-9\-])/';
+
+    /**
+     * @var bool
+     */
+    public $match = false;
+
+    /**
+     * @return array
+     */
+    public function getRequiredOptions()
+    {
+        return array();
+    }
+
+    /**
+     * @return string
+     */
+    public function validatedBy()
+    {
+        return 'Symfony\Component\Validator\Constraints\RegexValidator';
+    }
+}
