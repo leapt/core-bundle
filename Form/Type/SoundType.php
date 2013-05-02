27b27e52d47eee34f83f2703daee7e7cfaad264d
Edwin Hermans <edwin@snowcap.be>
2013-05-02 09:38:18 +0200

    New Sound FormType

---
+++ Form/Type/SoundType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Snowcap\CoreBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SoundType extends AbstractType
+{
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'snowcap_core_sound';
+    }
+
+    /**
+     * @return string
+     */
+    public function getParent()
+    {
+        return 'text';
+    }
+
+    /**
+     * @param \Symfony\Component\OptionsResolver\OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver
+            ->setDefaults(array(
+                'provider' => null
+            ))
+            ->setAllowedValues(array(
+                'provider' => array('soundcloud')
+            ));
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormView $view
+     * @param \Symfony\Component\Form\FormInterface     $form
+     * @param array                                     $options
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['provider'] = $options['provider'];
+    }
+}
