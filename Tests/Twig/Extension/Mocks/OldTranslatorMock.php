1645520ab39a69ff300468ea0c955dde219f2feb
jmsche <jonathan.scheiber@siep.be>
2019-11-26 09:43:19 +0100

    Fix OldTranslationMock

--- Tests/Twig/Extension/Mocks/OldTranslatorMock.php
+++ Tests/Twig/Extension/Mocks/OldTranslatorMock.php
@@ -4,7 +4,7 @@ namespace Leapt\CoreBundle\Tests\Twig\Extension\Mocks;
 
 class OldTranslatorMock extends AbstractTranslatorMock
 {
-    public function trans($id, array $parameters = [], string $domain = null, string $locale = null)
+    public function trans($id, array $parameters = [], $domain = null, $locale = null)
     {
         return $this->mockTrans($id, $parameters);
     }
