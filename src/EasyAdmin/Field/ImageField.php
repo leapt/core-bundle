071966792d4d06c7e30f537af960c3020d6ab7c7
jmsche <contact@jmsche.fr>
2022-07-12 15:55:29 +0200

    Add FileField & ImageField for EasyAdmin bundle

---
+++ src/EasyAdmin/Field/ImageField.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Leapt\CoreBundle\EasyAdmin\Field;
+
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FieldTrait;
+use Leapt\CoreBundle\Form\Type\ImageType;
+
+final class ImageField implements FieldInterface
+{
+    use FieldTrait;
+
+    public const OPTION_ALLOW_DELETE = 'allowDelete';
+    public const OPTION_ALLOW_DOWNLOAD = 'allowDownload';
+    public const OPTION_FILE_PATH = 'filePath';
+
+    public static function new(string $propertyName, ?string $label = null): self
+    {
+        return (new self())
+            ->setProperty($propertyName)
+            ->setLabel($label)
+            ->setTemplatePath('@LeaptCore/EasyAdmin/field/image.html.twig')
+            ->setFormType(ImageType::class)
+            ->addCssClass('field-image')
+            ->setFormTypeOption('required', false)
+            ->setCustomOption(self::OPTION_ALLOW_DELETE, true)
+            ->setCustomOption(self::OPTION_ALLOW_DOWNLOAD, true)
+            ->setCustomOption(self::OPTION_FILE_PATH, null)
+            ->addFormTheme('@LeaptCore/Form/bootstrap_5_layout.html.twig')
+        ;
+    }
+
+    public function setFilePath(string $path): self
+    {
+        $this->setCustomOption(self::OPTION_FILE_PATH, $path);
+
+        return $this;
+    }
+
+    public function allowDelete(): self
+    {
+        $this->setCustomOption(self::OPTION_ALLOW_DELETE, true);
+
+        return $this;
+    }
+
+    public function disallowDelete(): self
+    {
+        $this->setCustomOption(self::OPTION_ALLOW_DELETE, false);
+
+        return $this;
+    }
+
+    public function allowDownload(): self
+    {
+        $this->setCustomOption(self::OPTION_ALLOW_DOWNLOAD, true);
+
+        return $this;
+    }
+
+    public function disallowDownload(): self
+    {
+        $this->setCustomOption(self::OPTION_ALLOW_DOWNLOAD, false);
+
+        return $this;
+    }
+}
