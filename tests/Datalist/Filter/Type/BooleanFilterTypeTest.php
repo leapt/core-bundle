eb5f048fc5d8a572a751e551b5e6ca8e972f432a
jmsche <contact@jmsche.fr>
2022-09-15 15:39:15 +0200

    BooleanFilterType: docs, tests & changelog

--- tests/Datalist/Filter/Type/BooleanFilterTypeTest.php
+++ tests/Datalist/Filter/Type/BooleanFilterTypeTest.php
@@ -15,9 +15,37 @@ use Symfony\Component\Routing\RouterInterface;
 
 final class BooleanFilterTypeTest extends TestCase
 {
-    public function testFilter(): void
+    /**
+     * @dataProvider filterCasesProvider
+     */
+    public function testFilter(string $property, bool|string $value, array $expectedResult): void
     {
-        $items = [
+        $datasource = new ArrayDatasource($this->getItems());
+        $booleanFilterType = new BooleanFilterType();
+        $request = new Request([$property => $value]);
+
+        $datalistFactory = new DatalistFactory($this->createMock(FormFactoryInterface::class), $this->createMock(RouterInterface::class));
+        $datalistFactory->registerFilterType($booleanFilterType);
+        $datalist = $datalistFactory->createBuilder(DatalistType::class)
+            ->addFilter($property, BooleanFilterType::class)
+            ->getDatalist();
+        $datalist->setDatasource($datasource);
+        $datalist->bind($request);
+
+        self::assertSame($expectedResult, array_values(iterator_to_array($datalist->getIterator())));
+    }
+
+    public function filterCasesProvider(): iterable
+    {
+        yield 'boolean_true' => ['active', true, [['name' => 'Test 1', 'active' => true, 'used' => '1'], ['name' => 'Test 3', 'active' => true, 'used' => '0']]];
+        yield 'boolean_false' => ['active', false, [['name' => 'Test 2', 'active' => false, 'used' => '1']]];
+        yield 'string_1' => ['used', '1', [['name' => 'Test 1', 'active' => true, 'used' => '1'], ['name' => 'Test 2', 'active' => false, 'used' => '1']]];
+        yield 'string_0' => ['used', '0', [['name' => 'Test 3', 'active' => true, 'used' => '0']]];
+    }
+
+    private function getItems(): array
+    {
+        return [
             [
                 'name'   => 'Test 1',
                 'active' => true,
@@ -34,16 +62,5 @@ final class BooleanFilterTypeTest extends TestCase
                 'used'   => '0',
             ],
         ];
-        $datasource = new ArrayDatasource($items);
-
-        $request = new Request(['active' => true]);
-        $datalistFactory = new DatalistFactory($this->createMock(FormFactoryInterface::class), $this->createMock(RouterInterface::class));
-        $datalist = $datalistFactory->createBuilder(DatalistType::class)
-            ->addFilter('active', BooleanFilterType::class, [
-
-            ])
-            ->getDatalist();
-        $datalist->setDatasource($datasource);
-        $datalist->bind($request);
     }
 }
