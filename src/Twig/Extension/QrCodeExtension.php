7ca114935a986b66582399093e8c7135f13f7620
jmsche <jonathan.scheiber@wikipower.be>
2023-11-23 13:02:59 +0100

    Add support for endroid/qr-code v5

--- src/Twig/Extension/QrCodeExtension.php
+++ src/Twig/Extension/QrCodeExtension.php
@@ -6,7 +6,9 @@ namespace Leapt\CoreBundle\Twig\Extension;
 
 use Endroid\QrCode\Builder\Builder;
 use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
 use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\RoundBlockSizeMode;
 use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
 use Endroid\QrCode\Writer\PngWriter;
 use Twig\Extension\AbstractExtension;
@@ -27,15 +29,27 @@ final class QrCodeExtension extends AbstractExtension
             throw new \Exception('The "endroid/qr-code" package is required to use the "get_qr_code_from_string" Twig function. Try running "composer require endroid/qr-code".');
         }
 
+        if (class_exists(ErrorCorrectionLevel::class)) {
+            // endroid/qr-code v5 handles enums
+            $errorCorrectionLevel = ErrorCorrectionLevel::High;
+            $roundBlockSizeMode = RoundBlockSizeMode::Margin;
+        } else {
+            // while v4 handles classes
+            \assert(class_exists(ErrorCorrectionLevelHigh::class));
+            $errorCorrectionLevel = new ErrorCorrectionLevelHigh();
+            \assert(class_exists(RoundBlockSizeModeMargin::class));
+            $roundBlockSizeMode = new RoundBlockSizeModeMargin();
+        }
+
         $result = Builder::create()
             ->writer(new PngWriter())
             ->writerOptions([])
             ->data($qrCodeContent)
             ->encoding(new Encoding('UTF-8'))
-            ->errorCorrectionLevel(new ErrorCorrectionLevelHigh())
+            ->errorCorrectionLevel($errorCorrectionLevel)
             ->size($size)
             ->margin($margin)
-            ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
+            ->roundBlockSizeMode($roundBlockSizeMode)
             ->build();
 
         return $result->getDataUri();
